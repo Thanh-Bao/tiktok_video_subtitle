8011fff24b408faa3fca3849ae2eec51287cce3c
Bảo Bảo <thanh.bao@outlook.com>
2023-05-08 16:42:21 +0700

    add !

--- components/VideoScreen.js
+++ components/VideoScreen.js
@@ -24,7 +24,7 @@ export default function App() {
             <View style={tailwind('pt-12 items-center')}>
                 <View style={tailwind('bg-blue-200 px-3 py-1 rounded-full')}>
                     <Text style={tailwind('text-blue-800 font-semibold')}>
-                        Thêm sub vào đây
+                        Thêm sub vào đây!
                     </Text>
                 </View>
             </View>
